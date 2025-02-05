3b2d5b4dcfb561f3af1c268a64b8fbee6d0770f4
marva-sequioaat <marva@sequoiaat.com>
2025-02-05 07:17:59 +0000

    added name replce count to the function

--- namereplacer/fetchers/main.py
+++ namereplacer/fetchers/main.py
@@ -3,7 +3,7 @@ import requests
 import csv
 from namereplacer.constants import CSV_FILE,API_URL
 import re
-
+from collections import defaultdict
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(asctime)s - %(levelname)s - %(message)s"
@@ -56,15 +56,33 @@ def file_processor(input):
             
         with open(input,"r",encoding="utf-8") as f:
             lines=f.read()
-            
+            stats=defaultdict(int)
             for name,replace_name in replace_dic.items():
+                count = len(re.findall(re.escape(name), lines, flags=re.IGNORECASE))
+                stats[name] += count
                 lines=re.sub(re.escape(name),replace_name,lines,flags=re.IGNORECASE)
+            logger.info(f"name count:{stats}")
         with open("output.txt","w",encoding="utf-8") as f:
                 f.write(lines)
     except IOError as e:
         logger.error(f"Error reading/writing files: {e}")
         return None        
-            
+
+
+def word_count(input):
+    try:
+        with open(input, "r", encoding="utf-8") as f:
+            text = f.read()
+            words = text.split()
+            word_count = len(words)
+            unique_words = len(set(words))
+            logger.info(f"Total words: {word_count}")
+            logger.info(f"Unique words: {unique_words}")
+    except IOError as e:
+        logger.error(f"Error reading file: {e}")
+
+
+
 if __name__=="__main__":
 
     api_url=API_URL
